001d83d5eefd0db17ebc4feaa26b00e9cd8dd46a
ngustavin <gust4f3rn@gmail.com>
2020-12-21 09:43:23 -0300

    header styled

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,37 @@
 import {Container} from './styles'
 import React from 'react';
+import instagramLogo from '../../assets/instagram-logo.png'
+import profilePhoto from '../../assets/profile-image.jpeg'
+import { FiSend, FiHeart } from 'react-icons/fi'
+import { BiHomeAlt } from 'react-icons/bi'
+import { FaRegCompass } from 'react-icons/fa'
+
 
 // import { Container } from './styles';
 
 const Header: React.FC = () => {
   return (
         <Container>
-          <div className="logo">
-            <h1>Instagram</h1>
+          <div id ="header-wrapper">
+          <div className="image-container">
+            <img src={instagramLogo} alt="instagram-logo"/>
           </div>
           <input type="text"/>
           <div className="buttons">
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
-            <h1>Home</h1>
+            <BiHomeAlt size={24}/>
+            <FiSend size={24}/>
+            <FaRegCompass size={24}/>
+            <FiHeart size={24}/>
+            <img src={profilePhoto} alt="profile-photo"/>
+          </div>
           </div>
         </Container>
     );
 }
 
 export default Header;
+
+//FiSend
+//BiHomeAlt
+//FaRegCompass
+//FiHeart
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,6 +5,62 @@ export const Container = styled.div`
     width: 100%;
     height: 54px;
     display: flex;
-    justify-content: space-between;
-    padding: 0px 310px;
+    justify-content: center;
+    align-items: center;
+    outline: 0.6px solid rgba(0, 0, 0, 0.4);
+    background: #ffffff;
+
+    >#header-wrapper{
+        width: 975px;
+        height: 100%;
+        overflow-x: hidden;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        >.image-container{
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        overflow-y: hidden;
+        max-width: 360px;
+        width: 360px;
+        height:100%;
+
+        >img{
+            height: 76px;
+        }
+    }
+
+    > input{
+        height: 28px;
+        width: 215px;
+        background: #fafafa;
+        padding: 7px;
+        border-radius: 4px;
+    }
+
+    >.buttons{
+        display: flex;
+        flex-direction: row;
+        max-width: 360px;
+        width: 360px;
+        height:36px;
+        justify-content: flex-end;
+        align-items: center;
+        gap: 22px;
+
+        > img{
+            border-radius: 50%;
+            height: 24px;
+            width: 24px;
+        }
+    }
+}
+
+    
+
+    
+
+    
 `
